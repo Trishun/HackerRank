7784574810968797699e2b7c7f5603873e05bcc6
Trishun <piotr_dec@msn.com>
2022-10-23 11:57:46 +0200

    Creates test environment

---
+++ src/test/java/eu/ztsh/training/hackerrank/ReflectionHelper.java
@@ -0,0 +1,40 @@
+package eu.ztsh.training.hackerrank;
+
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Scanner;
+import eu.ztsh.training.hackerrank.SolutionClassDescription.FieldModifier;
+
+
+public class ReflectionHelper {
+
+    static {
+        try {
+            var lookup = MethodHandles.privateLookupIn(Field.class, MethodHandles.lookup());
+            MODIFIERS = lookup.findVarHandle(Field.class, "modifiers", int.class);
+            CAN_MODIFY_FINALS = true;
+        } catch (IllegalAccessException | NoSuchFieldException ex) {
+            CAN_MODIFY_FINALS = false;
+        }
+    }
+
+    static void reloadScanner(SolutionClassDescription description) throws NoSuchFieldException, IllegalAccessException {
+        // https://stackoverflow.com/a/3301720
+        var scannerField = description.hasModifier(FieldModifier.STATIC)
+                ? description.targetClass().getDeclaredField(description.fieldName())
+                : description.targetClass().getField(description.fieldName());
+        if (description.hasModifier(FieldModifier.PRIVATE)) {
+            scannerField.setAccessible(true);
+        }
+        if (description.hasModifier(FieldModifier.FINAL) && CAN_MODIFY_FINALS) {
+            MODIFIERS.set(scannerField, scannerField.getModifiers() & ~Modifier.FINAL);
+        }
+        scannerField.set(null, new Scanner(System.in));
+    }
+
+    private static VarHandle MODIFIERS;
+    private static boolean CAN_MODIFY_FINALS;
+
+}
